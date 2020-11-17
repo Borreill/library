df83d3367b8de9ff06a51cbddb51e1c30d232513
Borreill <inesborreill@yahoo.fr>
2020-11-17 10:17:45 +0100

    add authentification, path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase  from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'library';
+ constructor(){
+  var firebaseConfig = {
+    apiKey: "AIzaSyCELo_T76F_lQ__xMKuV2LItXX-BYvbsXs",
+    authDomain: "library-bcdf0.firebaseapp.com",
+    databaseURL: "https://library-bcdf0.firebaseio.com",
+    projectId: "library-bcdf0",
+    storageBucket: "library-bcdf0.appspot.com",
+    messagingSenderId: "635776091419",
+    appId: "1:635776091419:web:16c342517d66f2860a3fbe",
+    measurementId: "G-0XXCEW8GWM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,19 @@ import { HeaderComponent } from './header/header.component';
 import { AuthService } from './services/auth.service';
 import { AuthGuardService } from './services/auth-guard.service';
 import { BooksService } from './services/books.service';
-import {HttpClientModule} from '@angular/common//http';
+import { HttpClientModule } from '@angular/common//http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes =  [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', canActivate: [AuthGuardService], component: BookListComponent },
+  { path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent },
+  { path: 'books/view/:id', canActivate: [AuthGuardService], component: SingleBookComponent },
+  { path:'', redirectTo: 'books', pathMatch:'full' },
+  { path: '**', redirectTo: 'books' }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +39,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
